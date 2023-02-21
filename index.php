a0b9f8cfe08b48f6842f1878314e2ce8cf3b98a6
Nikol Ramírez <naramirez221@soy.sena.edu.co>
2023-02-20 20:52:25 -0500

    Version con suga

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mmm Mmm</title>
 </head>
 <body>
-    
+    <h1> El Peter Pan que aún no puede despertarse de su sueño </h1>
+    <h2>SUGA - D2</h2>
 </body>
 </html>
